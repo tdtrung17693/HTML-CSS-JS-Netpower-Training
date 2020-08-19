534346f6043862950df97b50c7c0b0baad21df4b
khoanguyen59 <phuongkhoa95ntp@gmail.com>
2020-08-19 14:44:07 +0700

    Import Bootstrap scss

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,8 @@ var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 //convert scss files to css
 gulp.task('sass',function(){
-return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'app/scss/*.scss'])
   .pipe(sass())
-  .pipe(gulp.dest('app/css'))
+  .pipe(gulp.dest('./app/css'))
   .pipe(browserSync.stream());
 });
